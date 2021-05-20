7b59aa3745033891df3b1307261189bf02cab52e
Luca <luca-heinrich@gmx.de>
2021-05-20 14:56:44 +0200

    - removed labels_db.sql since its not working. - added label details fetch functionality.

--- label_app/views.py
+++ label_app/views.py
@@ -45,5 +45,31 @@ def search_label(request):
 
 
 def label_details(request, guid):
-    return render(request, 'label_details.html')
+
+    sql_select_label = """
+                        SELECT lbl_guid, lbl_name, lbl_details, lbl_score_total, lbl_score_transparency, lbl_score_ecological, 
+                        lbl_score_quality, lbl_image_media_path
+                        FROM lbl_labels
+                        WHERE lbl_guid = %s
+                        """
+
+    with DBController() as db:
+        results = db.query(sql_select_label, (guid,))
+
+    results_dict = {}
+
+    if results:
+        lbl_guid, lbl_name, lbl_details, lbl_score_total, lbl_score_transparency, lbl_score_ecological, \
+        lbl_score_quality, lbl_image_media_path = results[0]
+
+        results_dict = {"guid": lbl_guid,
+                        "name": lbl_name,
+                        "details": lbl_details,
+                        "score_total": utils.get_total_score_display(lbl_score_total),
+                        "score_transparency": utils.get_total_score_display(lbl_score_transparency),
+                        "score_ecological": utils.get_total_score_display(lbl_score_ecological),
+                        "score_quality": utils.get_total_score_display(lbl_score_quality),
+                        "image_media_path": lbl_image_media_path}
+
+    return render(request, 'label_details.html', {"details": results_dict})
 
