a868dfbafaae9524ff7062ce69ae34598be629e5
Luca <luca-heinrich@gmx.de>
2021-05-21 15:12:06 +0200

    - minor css styling - added db sql statement

--- label_app/db_controller.py
+++ label_app/db_controller.py
@@ -1,4 +1,5 @@
 import os
+from django.templatetags.static import static
 
 from label_app import utils
 
@@ -46,4 +47,30 @@ class Database:
 
 
 class DBController(Database):
-    pass
+
+    def get_search_label_result(self, user_input, abs_static=False):
+
+        sql_search_label = """
+                                    SELECT lbl_guid, lbl_name, lbl_short_description, lbl_score_total, lbl_image_media_path
+                                    FROM lbl_labels
+                                    WHERE lbl_name LIKE %s
+                                    """
+        results = self.query(sql_search_label, params=('%%%s%%' % user_input,))
+        results_list = []
+
+        if results:
+            for found_label in results:
+                guid, name, short_description, score_total, image_media_path = found_label
+
+                label_info_dict = {"guid": guid,
+                                   "name": name,
+                                   "short_description": short_description[:80] + '...',
+                                   "score_total": score_total,
+                                   "score_total_display": utils.get_total_score_display(score_total),
+                                   "image_media_path": image_media_path if not abs_static else "/static/" + image_media_path,
+                                   "more_info_url": f"labels/{guid}"
+                                   }
+
+                results_list.append(label_info_dict)
+
+        return results_list
--- label_app/views.py
+++ label_app/views.py
@@ -1,47 +1,40 @@
 from django.shortcuts import render
 from label_app.db_controller import DBController
-from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 from label_app import utils
+import json
 
 def home(request):
     return render(request, 'home.html')
 
 
+@csrf_exempt
 def search_label(request):
 
-    if request.method == "POST":
-        user_searched = request.POST['label_search']
+    user_searched = None
+    search_results = []
 
-        sql_search_label = """
-                            SELECT lbl_guid, lbl_name, lbl_short_description, lbl_score_total, lbl_image_media_path
-                            FROM lbl_labels
-                            WHERE lbl_name LIKE %s
-                            """
+    if request.method == "POST":
+        post_data = json.loads(request.body.decode("utf-8"))
+        user_searched = post_data['label_search']
 
         with DBController() as db:
-            results = db.query(sql_search_label, params=('%%%s%%' % user_searched,))
+            search_results = db.get_search_label_result(user_searched, abs_static=request.get_host())
 
-        if results:
+        return JsonResponse({"data": search_results})
 
-            results_list = []
-            for found_label in results:
-                guid, name, short_description, score_total, image_media_path = found_label
+    elif request.method == "GET":
+        user_searched = request.GET['label_search']
 
-                label_info_dict = {"guid": guid,
-                                   "name": name,
-                                   "short_description": short_description[:80] + '...',
-                                   "score_total": score_total,
-                                   "score_total_display": utils.get_total_score_display(score_total),
-                                   "image_media_path": image_media_path,
-                                   "more_info_url": f"labels/{guid}"
-                                   }
+        with DBController() as db:
+            search_results = db.get_search_label_result(user_searched)
 
-                results_list.append(label_info_dict)
+        return render(request, 'search_label.html', {"results_list": search_results})
 
-        return render(request, 'search_label.html', {"results_list": results_list})
     else:
-        return render(request, 'search_label.html')
+        return render(request, 'home.html')
 
 
 def label_details(request, guid):
